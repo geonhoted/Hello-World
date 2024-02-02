c0a19bd31a2a1c984f72296a220b3f6a255af8a4
geonhoted <ted4124@gmail.com>
2024-02-02 23:28:24 +0900

    0201_1

--- 0201.py
+++ 0201.py
@@ -51,3 +51,56 @@ for i in range(st_n):
 
 result = " ".join(map(str, s))
 print(result)
+
+
+##
+sw_n = int(input())
+s = input().split()
+st_n = int(input())
+task = []
+for i in range(st_n):
+    task.append(input().split())
+
+for i in range(len(s)):
+    s[i] = int(s[i])
+
+for i in range(len(task)):
+    for j in range(2):
+        task[i][j] = int(task[i][j])
+
+def boy(n):
+    m = n
+    while m < sw_n:
+        if s[m - 1] == 0:
+            s[m - 1] = 1
+        else:
+            s[m - 1] = 0
+        m = m + n
+
+def girl(n):
+    m = n - 1
+    if s[m] == 0:
+        s[m] = 1
+    else:
+        s[m] = 0
+    a = 1
+    while True:
+        if (m - a) < 0 or (m + a) >= sw_n:
+            break
+        elif s[m - a] == s[m + a]:
+            if s[m - a] == 0:
+                s[m - a] = 1
+                s[m + a] = 1
+            else:
+                s[m - a] = 0
+                s[m + a] = 0
+            a += 1
+        else:
+            break
+
+result = " ".join(map(str, s))
+
+# 스위치 상태를 10개씩 나누어 출력
+for i in range(0, len(result), 10):
+    print(result[i:i+10])
+##
